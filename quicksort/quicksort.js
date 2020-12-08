e7078e694c42158588d4de35d605b5b7114dddea
Chuck Vaughan <chuck3774@hotmail.com>
2020-12-08 09:56:50 -0700

    Mostly completed quicksort

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -12,4 +12,41 @@
 
 
 var quicksort = function(array) {
+  var sortedArray = [];
+  var sort = function (arr) {
+    partialSort = [];
+    lessArray = [];
+    moreArray = [];
+    pivot = arr[Math.ceil((arr.length - 1) / 2)]
+    for (var i = 0; i < arr.length; i ++) {
+      if (arr[i] < pivot) {
+        lessArray.push(arr[i]);
+      }
+      if (arr[i] > pivot) {
+        moreArray.push(arr[i]);
+      }
+    }
+    console.log(lessArray);
+    if(lessArray[0] !== undefined) {
+      if (lessArray.length > 1) {
+       lessArray = sort(lessArray);
+      }
+    }
+    if(moreArray[0] !== undefined) {
+      if (moreArray.length > 1) {
+      moreArray = sort(moreArray);
+      }
+    }
+    partialSort = partialSort.concat(lessArray);
+    partialSort.push(pivot);
+    partialSort = partialSort.concat(moreArray);
+    return partialSort;
+  }
+  sortedArray = sort(array);
+  return sortedArray;
+
 };
+
+
+var poodle = [5, 4, 3, 2, 1, 8, 7, 9, 11, 57, 42];
+console.log(quicksort(poodle));
