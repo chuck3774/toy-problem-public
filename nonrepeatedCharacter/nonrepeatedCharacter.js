809f682a4089c1fc0bbbc8cbe699a00aa4baec5a
Chuck Vaughan <chuck3774@hotmail.com>
2020-12-14 09:57:53 -0700

    Completed nonrepeatedChar

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,36 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
+  var arrayString = Array.from(string);
+  var firstChar = undefined;
+  for (var i = 0; i < arrayString.length; i ++) {
+    if (!firstChar) {
+
+      var count = 0;
+      for (var j = 0; j < arrayString.length; j ++) {
+
+           if (arrayString[i] === arrayString[j]) {
+
+             count++;
+           }
+
+      }
+
+      console.log(count);
+      if (count === 1) {
+
+        firstChar = arrayString[i];
+
+
+    }
+
+    } else {
+      return firstChar;
+    }
+  }
+  return firstChar;
   // TODO: your solution here
 };
+
+console.log(firstNonRepeatedCharacter('ABA')); // => 'B'
+console.log(firstNonRepeatedCharacter('AACBDB')); // => 'C'
