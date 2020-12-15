6a14cd8524f92311c5e78051b07864b714168ec4
Chuck Vaughan <chuck3774@hotmail.com>
2020-12-15 09:49:28 -0700

    Completed Allanagrams

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -13,5 +13,20 @@
   */
 
 var allAnagrams = function(string) {
-  // Your code here.
+
+  if(string.length === 0) return [''];
+  var result = {};
+  string.split('').forEach(function(letter,i){
+        var remainingLetters = string.slice(0,i) + string.slice(i+1);
+
+         allAnagrams(remainingLetters).forEach(
+             function(anagram){
+         result[letter+anagram] = true;
+});
+});
+      return Object.keys(result);
+
 };
+
+var anagrams = allAnagrams('abc');
+console.log(anagrams);
