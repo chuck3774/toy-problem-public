ba03b96d9d60faa985687937c0c7a6c02ea12a52
Chuck Vaughan <chuck3774@hotmail.com>
2020-12-18 09:54:01 -0700

    Mostly Complete, each subset isn't sorted yet

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -7,15 +7,41 @@
  * powerSet("abc")
  * -> [ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ]
  *
- * Note: 
+ * Note:
  *  1. All characters in a subset should be sorted.
- *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same. 
- * 
+ *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same.
+ *
  * Example 2 :
- * 
+ *
  * powerSet("jump")
  * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
 
 var powerSet = function(str) {
+  var set = [''];
+
+  var subsetFinder = (string) => {
+
+      set.push(string);
+
+    for (var i = 0; i < str.length; i ++) {
+      var tempString = string;
+       if(str.indexOf(tempString[tempString.length - 1]) >= i) {
+         continue;
+       } else {
+          subsetFinder(tempString + str[i]);
+       }
+
+    }
+
+  }
+  for (var j = 0; j < str.length; j ++) {
+    subsetFinder(str[j]);
+  }
+
+  return set;
 };
+
+var test = powerSet('jump');
+console.log(test);
+
