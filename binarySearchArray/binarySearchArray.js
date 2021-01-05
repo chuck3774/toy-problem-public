152a695ac227c790c6a3894fce104ceb8d7e04d7
Chuck Vaughan <chuck3774@hotmail.com>
2021-01-05 09:45:53 -0700

    Completed binary Search Array

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,28 @@
  */
 
 var binarySearch = function (array, target) {
+  if (target > array[array.length -1]) {
+    return null;
+  }
+
+  let index = 0;
+  let length = array.length-1
+
+  while (index <= length) {
+    let mid = Math.floor((index + length)/2);
+
+    if (target > array[mid]) {
+      index = mid + 1;
+    } else if (target < array[mid]) {
+      length = mid -1;
+    } else {
+     return mid;
+    }
+  }
+  return index;
 };
+
+//TEST
+var index = binarySearch([1, 2, 3, 4, 5], 4);
+console.log(index);
 
