1ee9ac86ebf86e9f0e45d1e7ea26bc39acbc7719
Chuck Vaughan <chuck3774@hotmail.com>
2021-01-15 09:48:26 -0700

    Completed commonCharacters

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -13,5 +13,27 @@
 
 
 var commonCharacters = function(string1, string2) {
+let sharedString = '';
+let charCounter = {};
+
+for (let i = 0; i < string1.length; i ++) {
+  for (let j = 0; j < string2.length; j ++) {
+    if(string1[i] === string2[j]) {
+      var currentChar = string1[i];
+      if (charCounter[currentChar]) {
+          continue;
+      } else {
+        charCounter[currentChar] = 1;
+        sharedString += string1[i];
+      }
+    }
+  }
+}
+
+
+
+return sharedString;
   // TODO: Your code here!
 };
+
+console.log(commonCharacters('acexivou', 'aegihobu'));
