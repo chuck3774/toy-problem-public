80c868d84a3e80f34d316ab336f6b70e243ba2ef
Chuck Vaughan <chuck3774@hotmail.com>
2021-01-22 09:56:06 -0700

    Attempted LLintersection

--- linkedListIntersection/linkedListIntersection.js
+++ linkedListIntersection/linkedListIntersection.js
@@ -2,29 +2,29 @@
 *
 * Linked List Intersection
 *
-* Write a function linkedListIntersection that returns the node 
-* at which the intersection of two linked lists begins, 
+* Write a function linkedListIntersection that returns the node
+* at which the intersection of two linked lists begins,
 * or null if there is no such intersection.
-* 
+*
 * Example:
-* 
-* Given the following two linked lists list1 and list2, 
-* linkedListIntersection(list1,list2) should return D 
+*
+* Given the following two linked lists list1 and list2,
+* linkedListIntersection(list1,list2) should return D
 * as the node of intersection.
-* 
+*
 *    A → B → C
 *             ↘
 *               D → E → F
 *             ↗
 *        X → Y
-* 
-* Given the following two linked lists list1 and list2, 
-* linkedListIntersection(list1,list2) should return NULL 
+*
+* Given the following two linked lists list1 and list2,
+* linkedListIntersection(list1,list2) should return NULL
 * as there is no point of intersection.
-* 
+*
 *    A → B → C → D
 *    X → Y → Z
-* 
+*
 */
 
 // Helper function (do not edit)
@@ -37,7 +37,37 @@ function Node (val) {
 
 
 
-function linkedListIntersection(list1, list2) {
+function linkedListIntersection(originalList1, list1, list2, flips=0) {
+  // console.log(list1);
+  if (list1.value === list2.value) {
+    console.log(list1.value);
+    return list1.value;
+  }
+  else if (list1.next) {
+    return linkedListIntersection(originalList1, list1.next, list2, flips)
+  } else if (list2.next) {
+    console.log(flips);
+    if (flips === 0){
+      return linkedListIntersection(originalList1, list2.next, originalList1, 1)
+
+    } else {
+    return null;
+  }
   // TODO: Implement this function!
 }
+}
+
+var list1 = Node('X');
+list1.next = Node('Y');
+list1.next.next = Node('Z');
+list1.next.next.next = Node('D');
+list1.next.next.next.next = Node('E');
+
+var list2 = Node('A');
+list2.next = Node('B');
+list2.next.next = Node('C');
+list2.next.next.next = Node('D');
+list2.next.next.next.next = Node('E');
+
+console.log(linkedListIntersection(list1, list1, list2));
 
