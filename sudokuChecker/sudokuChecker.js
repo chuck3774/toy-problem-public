8999670fca0b43693b6aea2db33704f444d525ca
Chuck Vaughan <chuck3774@hotmail.com>
2021-01-29 09:44:14 -0700

    fixed syntax error

--- sudokuChecker/sudokuChecker.js
+++ sudokuChecker/sudokuChecker.js
@@ -20,7 +20,7 @@ Example input:
 
 
 
-function sudokuChecker(board) {
+
   function sudokuChecker(board) {
 
     function isSolved(s) {
@@ -63,18 +63,16 @@ function sudokuChecker(board) {
   }
 
 
-}
-
 //TEST
 
-// const test = "735814296\n
-// 896275314\n
-// 214963857\n
-// 589427163\n
-// 362189745\n
-// 471356982\n
-// 923541678\n
-// 648792531\n
-// 157638429";
+const test = ["735814296\n
+896275314\n
+214963857\n
+589427163\n
+362189745\n
+471356982\n
+923541678\n
+648792531\n
+157638429"];
 
-// console.log(sudokuChecker(test));
+console.log(sudokuChecker(test));
