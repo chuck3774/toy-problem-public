431f7e9843aa3e35400dd81bb3972fc987172be1
Chuck Vaughan <chuck3774@hotmail.com>
2021-02-03 09:42:11 -0700

    attempted editDistance

--- editDistance/editDistance.js
+++ editDistance/editDistance.js
@@ -3,5 +3,23 @@
 // Challenge: Do this in O(m x n) time, where m, n are the respective lengths of str1 and str2.
 
 function editDistance(str1, str2) {
-  // Your code here.
+  if (str1 === str2) {
+    return true;
+    return 0;
+  }
+
+  var numOfChanges = 0;
+
+  for (var i = str1.length - 1; i >= 0; i--) {
+    if (str1[i] !== str2[i]) {
+      str2[i] = str1[i];
+      numOfChanges++
+    }
+  }
+
+  return numOfChanges;
 }
+
+//TEST
+
+// console.log(editDistance('wednesday', 'sunday'));
