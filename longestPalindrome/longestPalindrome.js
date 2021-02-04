2077265fca31ff7c9c2c99707a8806584615b12d
Chuck Vaughan <chuck3774@hotmail.com>
2021-02-04 09:18:21 -0700

    Completed longestPalindrome

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -6,5 +6,38 @@
 * whitespace on each side of dad).
 */
 
-var longestPalindrome = function (string) {
-};
+function isPalindrome(s) {
+  var rev = s.split("").reverse().join("");
+  return s == rev;
+}
+var longestPalindrome = function (s) {
+
+    var maxp_length = 0,
+      maxp = '';
+
+    for(var i=0; i < s.length; i++) {
+    var subs = s.substr(i, s.length);
+
+    for(var j=subs.length; j>=0; j--) {
+      var sub_subs = subs.substr(0, j);
+      if (sub_subs.length <= 1)
+        continue;
+
+        //console.log('checking: '+ sub_subs);
+        if (isPalindrome(sub_subs)) {
+      //console.log('palindrome: '+ sub_subs);
+      if (sub_subs.length > maxp_length) {
+        maxp_length = sub_subs.length;
+        maxp = sub_subs;
+      }
+      }
+    }
+    }
+
+    //console.log(maxp_length, maxp);
+    return maxp;
+  };
+
+  //TEST
+
+  // console.log(longestPalindrome("My dad is a racecar athlete"));
