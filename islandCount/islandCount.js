319b2c230b49bde2b95ad67faa19184d17e2fd82
Chuck Vaughan <chuck3774@hotmail.com>
2021-02-12 09:42:32 -0700

    Attempted island count

--- islandCount/islandCount.js
+++ islandCount/islandCount.js
@@ -1,10 +1,37 @@
 /**
- * Given a string representation of a 2d map, return the number of islands in the map. 
+ * Given a string representation of a 2d map, return the number of islands in the map.
  * Land spaces are denoted by a zero, while water is denoted by a dot. Two land spaces
  * are considered connected if they are adjacent (but not diagonal).
  *
  */
 
 function countIslands(mapStr) {
-  // Your code here.
+  let counter = 0;
+
+  const islandSearch = (i, j) => {
+    if (
+      i >= 0 &&
+      j >= 0 &&
+      i < grid.length &&
+      j < grid[i].length &&
+      grid[i][j] === '1'
+    ) {
+      grid[i][j] = '0';
+      islandSearch(i + 1, j);
+      islandSearch(i, j + 1);
+      islandSearch(i - 1, j);
+      islandSearch(i, j - 1);
+    }
+  };
+
+  for (let i = 0; i < grid.length; i += 1) {
+    for (let j = 0; j < grid[i].length; j += 1) {
+      if (grid[i][j] === '1') {
+        counter += 1;
+        islandSearch(i, j);
+      }
+    }
+  }
+
+  return counter;
 }
