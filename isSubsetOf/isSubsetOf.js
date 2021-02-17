cce4585271d974cf1ab2ba3d3440cf44f8071bf4
Chuck Vaughan <chuck3774@hotmail.com>
2021-02-17 09:21:56 -0700

    isSubsetOf complete

--- isSubsetOf/isSubsetOf.js
+++ isSubsetOf/isSubsetOf.js
@@ -3,7 +3,7 @@
  * subset of an input array.  To simplify the problem, you can assume that neither
  * array will contain objects or arrays as elements within them.
  *
- * 
+ *
  * var a = ['commit','push']
  * a.isSubsetOf(['commit','rebase','push','blame']) // true
  *
@@ -11,7 +11,7 @@
  *
  * var b = ['merge','reset','reset']
  *
- * b.isSubsetOf(['reset','merge','add','commit']) // true 
+ * b.isSubsetOf(['reset','merge','add','commit']) // true
  *
  * See http://en.wikipedia.org/wiki/Subset for more on the definition of a
  * subset.
@@ -23,4 +23,27 @@
 
 Array.prototype.isSubsetOf = function (arr) {
   // your code here
+  let subset = true;
+  for (var i = 0; i < this.length; i ++) {
+    let count = 0;
+    for (var j = 0; j < arr.length; j ++) {
+      if (this[i] === arr[j]) {
+        count++
+      }
+    }
+    if (count === 0) {
+      subset = false;
+    }
+  }
+
+  return subset;
 };
+
+//TEST
+
+// var a = ['commit','push'];
+//  console.log(a.isSubsetOf(['commit','rebase','push','blame']));
+// var b = ['merge','reset','reset'];
+// console.log(b.isSubsetOf(['reset','merge','add','commit']));
+// var c = ['dog','reset','reset'];
+// console.log(c.isSubsetOf(['reset','merge','add','commit']));
